f1bda69ef6c944d92ccbf58c959d40ef2d5e6fdf
malinapavel <malina.pavel@student.upt.ro>
2022-10-31 12:34:22 +0200

    Registration + newletter backend integrated

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class NewsletterSubscribers(models.Model):
+    email = models.EmailField(null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
--- core/views.py
+++ core/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from . models import NewsletterSubscribers
 
 # Create your views here.
 def index(request):
-    #return HttpResponse('<p>Hi</p>')
+    if request.method == 'POST':
+        email = request.POST['email-sub']
+        if '@' in email:
+            if NewsletterSubscribers.objects.filter(email = email).exists():
+                messages.info(request, 'You already subscribed to the newsletter.')
+                
+            else: 
+                messages.info(request, 'Thank you, we are glad that you enjoy WeirdoWorld!💗')
+                NewsletterSubscribers.objects.create(email=email)
+
     return render(request, 'index.html')
 
 def index2(request):
@@ -16,7 +28,28 @@ def contact(request):
     return render(request, 'contact.html')
 
 def sign_up(request):
-    return render(request, 'sign-up.html')
+    if request.method == 'POST':
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        username = request.POST['usernamee']
+        password = request.POST['password']
+        cpassword = request.POST['confirm_password']
+
+        if password == cpassword:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'Sorry, but this username already exists. 😭')
+                return render(request, 'sign-up.html')
+            else:
+                user = User.objects.create_user(first_name=firstname, last_name=lastname, username=username, password=password)
+                user.save()
+                messages.info(request, 'Account created successfully! 😃 Please try now to log-in.')
+                return render(request, 'sign-up.html')
+        else:
+            messages.info(request, 'Sorry, but the passwords do not match. 😭')
+            return render(request, 'sign-up.html')
+
+    else:
+        return render(request, 'sign-up.html')
 
 def all_news(request):
     return render(request, 'all-news.html')
